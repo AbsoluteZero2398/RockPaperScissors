1d3106325ee5e0b4ba7762b89e5cbd5ff1141d71
AbsoluteZero2398 <117696847+AbsoluteZero2398@users.noreply.github.com>
2023-01-13 15:36:06 -0500

    removed alert debugging messages and changed out console log messages in switch for alerts

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const choices = ["rock", "paper", "scissors"];
 const playerSelection = playerChoice();
 const computerSelection = computerChoice();
-alert("variables initialized");
+//alert("variables initialized");
 
 function game(){
 playRound();
 }
-alert("first function");
+//alert("first function");
 
 function playerChoice(){
     let input = prompt("Type Rock, Paper, or Scissors");
@@ -17,50 +17,50 @@ function playerChoice(){
     console.log(input);
     }
 
-alert("second function");
+//alert("second function");
 
     function computerChoice(){
     return choices[Math.floor(Math.random()*choices.length)]
     }
 
-alert("third function");
+//alert("third function");
 
 function playRound(playerSelection, computerSelection){
 
 switch(true){
 
 case playerSelection == "rock" && computerSelection == 0:
-    console.log("Tie");
+    alert("Tie");
     break;
 case playerSelection == "paper" && computerSelection == 1:
-    console.log("Tie");
+    alert("Tie");
     break;
 case playerSelection == "scissors" && computerSelection == 2:
-    console.log("Tie");
+    alert("Tie");
     break;
 case playerSelection == "rock" && computerSelection == 2:
-    console.log("Win");
+    alert("Win");
     break;
 case playerSelection == "paper" && computerSelection == 0:
-    console.log("Win");
+    alert("Win");
     break;
 case playerSelection == "scissors" && computerSelection == 1:
-    console.log("Win");
+    alert("Win");
     break;
 case playerSelection == "rock" && computerSelection == 1:
-    console.log("Lose");
+    alert("Lose");
     break;
 case playerSelection == "paper" && computerSelection == 2:
-    console.log("Lose");
+    alert("Lose");
     break;
 case playerSelection == "scissors" && computerSelection == 0:
-    console.log("Lose");
+    alert("Lose");
     break;
 
     }
 }
 
-alert("fourth function, switch has completed");
+//alert("fourth function, switch has completed");
 
 function validateInput(input){
     if(choices.includes(choice)){
@@ -70,6 +70,6 @@ function validateInput(input){
     }
 }
 
-alert("fifth function");
+//alert("fifth function");
 
 game();
