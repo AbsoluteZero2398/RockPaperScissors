a01140b17494fdecffbc83434e6508d565479fc8
AbsoluteZero2398 <117696847+AbsoluteZero2398@users.noreply.github.com>
2023-01-13 12:24:50 -0500

    added switch method to index.js

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const choices = ["rock", "paper", "scissors"];
+const playerSelection = playerChoice();
+const computerSelection = computerChoice();
 
 function game(){
 playRound();
 }
 
-function playRound(){
-const playerSelection = playerChoice();
-const computerSelection = computerChoice();
+function playRound(playerSelection, computerSelection){
+
+switch(true){
+
+case playerSelection == "rock" && computerSelection == 0:
+    console.log("Tie");
+    break;
+case playerSelection == "paper" && computerSelection == 1:
+    console.log("Tie");
+    break;
+case playerSelection == "scissors" && computerSelection == 2:
+    console.log("Tie");
+    break;
+case playerSelection == "rock" && computerSelection == 2:
+    console.log("Win");
+    break;
+case playerSelection == "paper" && computerSelection == 0:
+    console.log("Win");
+    break;
+case playerSelection == "scissors" && computerSelection == 1:
+    console.log("Win");
+    break;
+case playerSelection == "rock" && computerSelection == 1:
+    console.log("Lose");
+    break;
+case playerSelection == "paper" && computerSelection == 2:
+    console.log("Lose");
+    break;
+case playerSelection == "scissors" && computerSelection == 0:
+    console.log("Lose");
+    break;
+
+    }
 }
 
 function playerChoice(){
